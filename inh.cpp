d96eb260620520742ee95a84e542365f6f51f358
tejas <tejasshah@gmail.com>
2022-06-19 16:12:08 +0530

    inh

--- inh.cpp
+++ inh.cpp
@@ -2,8 +2,56 @@
 
 using namespace std;
 
+class Computer{
+    private :
+        int ram;
+
+    public:
+        void getDataForComputer();
+        void printData();
+
+};
+//
+void Computer::getDataForComputer(){
+    cout<<"Enter ram cap";
+    cin>>ram;
+}
+void Computer::printData(){
+    cout<<ram;
+}
+
+class Laptop:public Computer{
+    private:
+        int ssd;
+
+    public:
+        void getData();
+        void printData();
+} ;
+//
+
+void Laptop::getData(){
+    cout<<"Enter SSD cap";
+    cin>>ssd;
+}
+void Laptop::printData(){
+    cout<<ssd;
+}
+
+
+
+
+
 
 int main(){
 
+    //Computer c;
+    //c.getData();
+    //c.printData();
+
+    Laptop l;
+    l.getData();
+    l.printData();
+    l.getDataForComputer();//computer
     return 0;
 }

57fcf02b8a69183c0ed1ca5d80c56026ac87963f
tejas <tejasshah@gmail.com>
2022-07-10 15:09:12 +0530

    override

---
+++ bank.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+
+using namespace std; 
+class Bank{
+
+    public:
+        void transfer(int amt,char upi[]){
+        }
+        void transfer(int amt,char ifsccode[],char savingAccnum[]){
+        }
+        void transfer(int amt,char neft[],char bank[],char accNum[],char ifsc[]){
+        }
+        //dc 
+        //cc
+        //xx
+        //xx
+        //emi 
+        //
+
+
+
+
+};
+
+int main(){
+    Bank b;
+    b.transfer(2000);
+}
